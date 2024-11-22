90428b28d0dd28e5bec265f5dc6edd95e5fc9b52
Robin <126180359+Rutcha007@users.noreply.github.com>
2024-11-22 10:16:51 +0100

    strukturen erzeugt

--- exercise-006/linkedlist.h
+++ exercise-006/linkedlist.h
@@ -1,6 +1,17 @@
 #ifndef LinkedList_H //include guard
 #define LinkedList_H
 
+typedef struct ListNode
+{
+    unsigned int data;
+    struct ListNode* pNext;
+}ListNode_t;
 
+typedef struct List
+{
+    struct ListNode* pHead;
+    struct ListNode* pTail;
+    unsigned int size;
+}List_t;
 
 #endif
